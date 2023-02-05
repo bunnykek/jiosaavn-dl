f9daeba377220f88f644700735e54ff88cf84a8c
bunny <67633271+bunnykek@users.noreply.github.com>
2023-02-05 16:14:09 +0530

    Rewritten the code
    Changed the API endpoints. Now using web endpoints.
    No web-scraping therefore a bit faster.
    Implemented OOP

--- jiosaavn.py
+++ jiosaavn.py
@@ -1,22 +1,24 @@
 # jiosaavn-dl
 # made by bunny
 
+import json
 import requests
 from html import unescape
 from sanitize_filename import sanitize
 import re
 import os
-import sys
 import argparse
 from mutagen.mp4 import MP4, MP4Cover
 
 
-# Jiosaavn API endpoints, credits goes to cyberboysumanjay
-song_api = "https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids="
-album_api = "https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&albumid="
-playlist_api = "https://www.jiosaavn.com/api.php?__call=playlist.getDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&listid="
+song_api = "https://www.jiosaavn.com/api.php?__call=webapi.get&token={}&type=song"
+album_api = "https://www.jiosaavn.com/api.php?__call=webapi.get&token={}&type=album"
+playlist_api = "https://www.jiosaavn.com/api.php?__call=webapi.get&token={}&type=playlist&_format=json"
 lyrics_api = "https://www.jiosaavn.com/api.php?__call=lyrics.getLyrics&ctx=web6dot0&api_version=4&_format=json&_marker=0%3F_marker%3D0&lyrics_id="
-
+album_song_rx = re.compile("https://www\.jiosaavn\.com/(album|song)/.+?/(.+)")
+playlist_rx = re.compile("https://www\.jiosaavn\.com/s/playlist/.+/(.+)")
+cdn_track_tx = re.compile("https://preview\.saavncdn\.com/(\d+)/(.+)_96_p.mp4")
+json_rx = re.compile("({.+})")
 
 logo = """
            /$$            /$$$$$$                                                         /$$ /$$
@@ -38,96 +40,163 @@ def clear() -> None:
     os.system('clear' if os.name == 'posix' else 'cls')
 
 
-# returns album/song/playlist id
-def get_id(response, identity):
-    return(re.search(f'\"{identity}\":\".*?\"', response.text).group().replace(f'\"{identity}\":\"', "").replace("\"", ""))
+class Jiosaavn:
+    def __init__(self) -> None:
+        self.session = requests.Session()
 
-# album json handler
-def album_json_handler(json, album_path):
-    for song in json["songs"]:
-        download_song(json["songs"].index(song)+1, song, album_path,
-                      json["primary_artists"], len(json['songs']))
+    # Tags metadata to a track
+    def tagger(self, json, song_path, album_artist, album_path, pos=1, total=1):
+        audio = MP4(song_path)
+        audio["\xa9nam"] = sanitize(unescape(json["song"]))
+        audio["\xa9alb"] = sanitize(unescape(json["album"]))
+        audio["\xa9ART"] = sanitize(unescape(json["primary_artists"]))
+        audio["\xa9wrt"] = sanitize(unescape(json["music"] ))
+        audio["aART"] = album_artist if album_artist else sanitize(unescape(json["primary_artists"]))
+        audio["\xa9day"] = json["release_date"]  # json["year"]
+        audio["----:TXXX:Record label"] = bytes(json["label"], 'UTF-8')
+        audio["cprt"] = json["copyright_text"]
+        audio["----:TXXX:Language"] = bytes(json["language"].title(), 'UTF-8')
+        audio["rtng"] = [2 if json["explicit_content"] == 0 else 4]
+        # audio["----:TXXX:URL"] = bytes(json["album_url"], 'UTF-8')
+        audio["trkn"] = [(pos, total)]
 
-# playlist json handler :/
-def playlist_json_handler(json, playlist_path):
+        # if the song has lyrics then tag else skip
+        if(json["has_lyrics"] == "true"):
+            lyric_json = self.session.get(lyrics_api + json["id"]).json()
+            audio["\xa9lyr"] = lyric_json["lyrics"].replace("<br>", "\n")
 
-    for song in json['songs']:
+        # cover artwork tag
+        with open(os.path.join(album_path, "cover.jpg"), "rb") as f:
+            audio["covr"] = [MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_JPEG)]
 
-        # download the artwork
-        with open(os.path.join(playlist_path, "cover.jpg"), "wb") as f:
-            f.write(requests.get(song["image"].replace("150", "500")).content)
+        #featured artists
+        if len(json['featured_artists']) > 1:
+            audio["----:TXXX:Featured artists"] = bytes(json["featured_artists"], 'UTF-8')
 
-        # download the track
-        download_song(json['songs'].index(
-            song)+1, song, playlist_path, song['primary_artists'], len(json['songs']))
+        #singers
+        if len(json['singers']) > 1:
+            audio["----:TXXX:Singers"] = bytes(json["singers"], 'UTF-8')
 
-        # deleting the artwork
-        os.remove(os.path.join(playlist_path, "cover.jpg"))
+        #starring
+        if len(json['starring']) > 1:
+            audio["----:TXXX:Starring"] = bytes(json["starring"], 'UTF-8')
+
+        audio.pop("©too")
+
+        audio.save()  # tagging done
 
 
-# song downloader
-def download_song(pos, json, path, album_artists, total=1):
+    def processAlbum(self, album_id):
+        
+        # sanitization
+        album_json = self.session.get(album_api.format(album_id)).text
+        album_json = json.loads(json_rx.search(album_json).group(1))
+        
+        album_name = sanitize(unescape(album_json['title']))
+        album_artist = album_json['primary_artists']
+        total_tracks = len(album_json['songs'])
+        year = str(album_json['year'])
 
-    # sanitize
-    json['song'] = sanitize(unescape(json['song']))
-    json["album"] = sanitize(unescape(json["album"]))
-    json["primary_artists"] = sanitize(unescape(json["primary_artists"]))
-    json["music"] = sanitize(unescape(json["music"]))
+        album_info = f"\n\
+                    Album info:\n\
+                    Album name       : {album_name}\n\
+                    Album artists    : {album_artist}\n\
+                    Year             : {year}\n\
+                    Number of tracks : {total_tracks}\n"
+        print(album_info)
 
-    # setting the song download path
-    song_path = os.path.join(path, f"{str(pos).zfill(2)}. {json['song']}.m4a")
+        song_pos = 1
+        for song in album_json['songs']:
+            song_id = album_song_rx.search(song['perma_url']).group(2)
+            self.processTrack(song_id, album_artist, song_pos, total_tracks)
+            song_pos+=1
+    
+    def processTrack(self, song_id, album_artist = None, song_pos = 1, total_tracks = 1, isPlaylist = False):
+        
+        metadata = self.session.get(song_api.format(song_id)).text
+        metadata = json.loads(json_rx.search(metadata).group(1))
+        # print(metadata.keys())
+        song_json = metadata[f'{list(metadata.keys())[0]}']
 
-    # checking if the song already exists in the directory
-    if(os.path.exists(song_path)):
-        print(f"{json['song']} already downloaded.")
-    else:
-        print(f"\nDownloading : {str(pos).zfill(2)}. {json['song']}...")
+        # sanitize
+        primary_artists = album_artist if album_artist else sanitize(unescape(song_json["primary_artists"]))
+        track_name = sanitize(unescape(song_json['song']))
+        album = sanitize(unescape(song_json['album']))
+        year = str(unescape(song_json['year']))
 
-        # checking if the song is available in the region, if yes then proceed to download else prompt the unavailability
-        if 'media_preview_url' in json:
-            link = "https://sdlhivkcdnems04.cdnsrv.jio.com/jiosaavn.cdn.jio.com" + \
-                (re.search(r"/\d{3}/\w+_96", json['media_preview_url'].replace(
-                    "\\", ""))).group().replace("_96", "_320.mp4")
-
-            # download the song
-            with open(song_path, "wb") as f:
-                f.write(requests.get(link).content)
-
-            print("Tagging metadata...")
-            tagger(
-                json, path, f"{str(pos).zfill(2)}. {json['song']}.m4a", album_artists, pos, total)
-            print("Done.")
+        # setting up the song directory
+        if isPlaylist:
+            folder_name = isPlaylist
         else:
-            print("\nTrack unavailable in your region!")
+            folder_name = f"{primary_artists if primary_artists.count(',') < 2 else 'Various Artists'} - {album} [{year}]"
+        song_name = f"{str(song_pos).zfill(2)}. {track_name}.m4a"
+        
+        album_path = os.path.join("Downloads", folder_name)
+        song_path = os.path.join("Downloads", folder_name, song_name)
 
+        try:
+            os.makedirs(album_path)
+        except:
+            pass
 
-# Tags metadata to a track
-def tagger(json, path, name, album_artists, pos=1, total=1):
-    audio = MP4(os.path.join(path, name))
-    audio["\xa9nam"] = json["song"]
-    audio["\xa9alb"] = json["album"]
-    audio["\xa9ART"] = json["primary_artists"]
-    audio["\xa9wrt"] = json["music"]
-    audio["aART"] = album_artists
-    audio["\xa9day"] = json["release_date"]  # json["year"]
-    audio["----:TXXX:Record label"] = bytes(json["label"], 'UTF-8')
-    audio["cprt"] = json["copyright_text"]
-    #audio["----:TXXX:Release date"] = bytes(json["release_date"], 'UTF-8')
-    audio["----:TXXX:Language"] = bytes(json["language"].title(), 'UTF-8')
-    audio["rtng"] = [2 if json["explicit_content"] == 0 else 4]
-    audio["----:TXXX:URL"] = bytes(json["album_url"], 'UTF-8')
-    audio["trkn"] = [(pos, total)]
+        song_info = f"\n\
+                    Track info:\n\
+                    Song name      : {song_json['song']}\n\
+                    Artist(s) name : {song_json['primary_artists']}\n\
+                    Album name     : {song_json['album']}\n\
+                    Year           : {song_json['year']}\n"
 
-    # if the song has lyrics then tag else skip
-    if(json["has_lyrics"] == "true"):
-        lyric_json = requests.get(lyrics_api + json["id"]).json()
-        audio["\xa9lyr"] = lyric_json["lyrics"].replace("<br>", "\n")
+        print(song_info)
 
-    # cover artwork tag
-    with open(os.path.join(path, "cover.jpg"), "rb") as f:
-        audio["covr"] = [MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_JPEG)]
-    audio.pop("©too")
-    audio.save()  # tagging done
+        # checking if the cover already exists
+        if not os.path.exists(os.path.join(album_path, "cover.jpg")) or isPlaylist:
+            print("\nDownloading the cover...")
+            with open(os.path.join(album_path, "cover.jpg"), "wb") as f:
+                f.write(self.session.get(
+                    song_json["image"].replace("150", "500")).content)
+
+        # checking if the song already exists in the directory
+        if(os.path.exists(song_path)):
+            print(f"{song_name} already downloaded.")
+        else:
+            print(f"Downloading : {song_name}...")
+
+            # checking if the song is available in the region, if yes then proceed to download else prompt the unavailability
+            if 'media_preview_url' in song_json:
+                threeDigits, cdnTrackid = cdn_track_tx.search(song_json['media_preview_url']).groups()
+                cdnURL = f"https://aac.saavncdn.com/{threeDigits}/{cdnTrackid}_320.mp4"
+
+                # download the song
+                with open(song_path, "wb") as f:
+                    f.write(self.session.get(cdnURL).content)
+
+                print("Tagging metadata...")
+
+                self.tagger(song_json, song_path, album_artist ,album_path, song_pos, total_tracks)
+                print("Done.")
+            else:
+                print("\nTrack unavailable in your region!")
+
+    def processPlaylist(self, playlist_id):
+        # json
+
+        playlist_json = self.session.get(playlist_api.format(playlist_id)).text
+        playlist_json = json.loads(json_rx.search(playlist_json).group(1))
+
+        playlist_name = playlist_json['listname']
+        total_tracks = int(playlist_json['list_count'])
+        playlist_path = f"Playlist - {playlist_name}"
+        playlist_info = f"\n\
+                        Playlist info:\n\
+                        Playlist name    : {playlist_name}\n\
+                        Number of tracks : {total_tracks}\n"
+        print(playlist_info)
+        
+        song_pos = 1
+        for song in playlist_json['songs']:
+            song_id = album_song_rx.search(song['perma_url']).group(2)
+            self.processTrack(song_id, None, song_pos, total_tracks, playlist_path)
+            song_pos+=1
 
 
 if __name__ == "__main__":
@@ -141,111 +210,19 @@ if __name__ == "__main__":
 
     url = args.url
 
+    jiosaavn = Jiosaavn()
+
     # handles album URL
-    if("/album/" in url):
+    if("/album/" in url or "/song/" in url):
 
-        response = requests.get(url)
-        album_id = get_id(response, "album_id")
+        kind, id_ = album_song_rx.search(url).groups()
 
-        # getting json
-        album_json = requests.get(album_api+album_id).json()
-
-        # sanitization
-        album_json["primary_artists"] = sanitize(unescape(album_json["primary_artists"]))
-        album_json['title'] = sanitize(unescape(album_json['title']))
-
-        # setting the album path
-        album_path = os.path.join(sys.path[0], "Downloads", (album_json["primary_artists"] if album_json["primary_artists"].count(
-            ",") < 2 else "Various Artists") + f" - {album_json['title']} [{album_json['year']}]")
-        try:
-            os.makedirs(album_path)
-        except:
-            pass
-
-        album_info = f"\n\
-                    Album info:\n\
-                    Album name       : {album_json['title']}\n\
-                    Album artists    : {album_json['primary_artists']}\n\
-                    Release date     : {album_json['release_date']}\n\
-                    Album ID         : {album_json['albumid']}\n\
-                    Number of tracks : {len(album_json['songs'])}\n"
-        print(album_info)
-
-        # checking if the cover already exists inside the album folder
-        if not os.path.exists(os.path.join(album_path, "cover.jpg")):
-            # download the cover
-            print("\nDownloading the cover...")
-            with open(os.path.join(album_path, "cover.jpg"), "wb") as f:
-                f.write(requests.get(
-                    album_json["image"].replace("150", "500")).content)
-
-        album_json_handler(album_json, album_path)
-
-    # handles song URL
-    elif("/song/" in url):
-        song_response = requests.get(url)
-        song_id = get_id(song_response, "song_id")
-        song_json = requests.get(song_api+song_id).json()
-
-        # getting json
-        song_json = song_json[f'{song_id}']
-
-        # sanitize
-        song_json["primary_artists"] = sanitize(unescape(song_json["primary_artists"]))
-        song_json['song'] = sanitize(unescape(song_json['song']))
-        song_json['music'] = sanitize(unescape(song_json['music']))
-        song_json['album'] = sanitize(unescape(song_json['album']))
-        song_json['has_lyrics'] = unescape(song_json['has_lyrics'])
-
-        # setting up the song directory
-        song_path = os.path.join(sys.path[0], "Downloads", (song_json["primary_artists"] if song_json["primary_artists"].count(
-            ",") < 2 else "Various Artists") + f" - {song_json['song']} [{song_json['year']}]")
-        try:
-            os.makedirs(song_path)
-        except:
-            pass
-
-        song_info = f"\n\
-                    Track info:\n\
-                    Song name      : {song_json['song']}\n\
-                    Artist(s) name : {song_json['primary_artists']}\n\
-                    Composer       : {song_json['music']}\n\
-                    Album name     : {song_json['album']}\n\
-                    Year           : {song_json['year']}\n\
-                    Has lyrics?    : {song_json['has_lyrics']}\n"
-
-        print(song_info)
-
-        # checking if the cover already exists
-        if not os.path.exists(os.path.join(song_path, "cover.jpg")):
-            print("\nDownloading the cover...")
-            with open(os.path.join(song_path, "cover.jpg"), "wb") as f:
-                f.write(requests.get(
-                    song_json["image"].replace("150", "500")).content)
-
-        download_song(1, song_json, song_path, song_json['primary_artists'])
-
-    # handles playlist URL
-    elif("/playlist/" in url):
-        playlist_response = requests.get(url)
-        playlist_id = get_id(playlist_response, "listid")
-
-        # json
-        playlist_json = requests.get(playlist_api+playlist_id).json()
-        playlist_path = os.path.join(
-            sys.path[0], "Downloads", f"Playlist - {playlist_json['listname']}")
-        playlist_info = f"\n\
-                        Playlist info:\n\
-                        Playlist name    : {playlist_json['listname']}\n\
-                        Number of tracks : {playlist_json['list_count']}\n"
-        print(playlist_info)
-        # creating playlist directory
-        try:
-            os.makedirs(playlist_path)
-        except:
-            pass
-
-        playlist_json_handler(playlist_json, playlist_path)
-
+        if kind == 'song':
+            jiosaavn.processTrack(id_, None, 1, 1)
+        elif kind == 'album':
+            jiosaavn.processAlbum(id_)
+    elif '/playlist/' in url:
+        playlist_id = playlist_rx.search(url).group(1)
+        jiosaavn.processPlaylist(playlist_id)
     else:
         print("Please enter a valid link!")
